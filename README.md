29443c4caa7697bc19ca001c7b7dad9616370c35
tsakiris <tsakiris@uth.gr>
2019-04-06 16:28:38 +0300

    Added sublime settings, replaced mv commands with cp in order to keep files to the dir after the setup

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
 # configurations
+
 This repository holds all the required pieces to install my packages and my configurations on a new Linux machine.
 
 ## Instructions
-__Method 1__
-* Clone the repository and enter the directory
-* Give executable rights to the installer with **chmod +x installer.sh**
-* Start the installer with **./installer.sh**
 
-__Method 2__
-* Download the installer_with_gdrive.sh
-* Give executable rights to the installer if needed with **chmod +x installer_with_gdrive.sh**
-* Start the installer with **./installer_with_gdrive.sh**
+* Clone the repository and enter the directory
+* Give executable rights to the installer if needed with **chmod +x installer.sh**
+* Start the installer with **./v2_installer.sh**
 
 ### TODOS
+
 - [x] Installer with google drive links-urls
-- [ ] Installer with local files from the repository
+- [x] Installer with local files from the repository
+- [ ] VSCode extensions and settings
+
+#### Important Notice
+
+./installer.sh is `deprecated` and shouldn't be used.
--- v2_installer.sh
+++ v2_installer.sh
@@ -3,9 +3,9 @@
 # This script provides an easy way to install my preferred packages along with my configurations.
 # Author: Tsakiris Tryfon
 # Version: 2.0
-# @This version uses local files from github repository.
+# This version uses local files from github repository.
 
-# -------------------------------------------------------- Font commands ----------------------------------------------------------
+# -------------------------------------------------------- Font commands -----------------------------------------------
 bold=$(tput bold)
 start_underline=$(tput smul)
 end_underline=$(tput rmul)
@@ -13,12 +13,7 @@ black=$(tput setaf 0)
 red=$(tput setaf 1)
 reset=$(tput sgr0)
 
-# ---------------------------------------------------------- Functions -------------------------------------------------------------
-function _movetoroot() {
-	echo "Moving $1 to '~/' directory..."
-	mv $1 ~/.$1 -v --backup=numbered
-}
-
+# ---------------------------------------------------------- Functions -------------------------------------------------
 function _backup() {
 	echo "Backing up $1 ..."
 	cp $1 $1 -v --force --backup=numbered
@@ -47,7 +42,8 @@ function _prompt() {
 
 function _checkfile() {
 	if [ ! -f $1 ]; then
-		echo "${bold}${red}ERROR${reset}: Can't find ${1} in this directory. Installer needs all the contents of the github repository."
+		echo "${bold}${red}ERROR${reset}: Can't find ${1} in this directory. Installer needs all the contents of" \
+		"the github repository."
 		echo "You should issue the command: > git clone https://github.com/tsakirist/configurations.git"
 		exit 1
 	fi
@@ -55,22 +51,22 @@ function _checkfile() {
 
 function _curl() {
 	if ! command -v curl > /dev/null 2>&1; then
-		echo "Installing ${bold}${red}cURL${reset}..."
+		echo "Installing ${bold}${red}cURL${reset} ..."
 		sudo apt update && sudo apt install -y curl
 	fi
 }
 
 function _git() {
 	if ! command -v git > /dev/null 2>&1; then
-		echo "Installing ${bold}${red}git${reset}..."
+		echo "Installing ${bold}${red}git${reset} ..."
 		sudo apt update && sudo apt install -y git
 	fi
 }
 
 function _gitconfig() {
 	_checkfile gitconfig
-	echo "Setting ${bold}${red}.gitconfig${reset}..."
-	_movetoroot gitconfig
+	echo "Setting ${bold}${red}.gitconfig${reset} ..."
+	cp -v --backup=numbered gitconfig ~/.gitconfig
 }
 
 function _gitsofancy() {
@@ -82,31 +78,31 @@ function _gitsofancy() {
 
 function _bashaliases() {
 	_checkfile bash_aliases
-	echo "Setting ${bold}${red}.bash_aliases${reset}..."
-	_movetoroot bash_aliases
+	echo "Setting ${bold}${red}.bash_aliases${reset} ..."
+	cp -v --backup=numbered bash_aliases ~/.bash_aliases
 }
 
 function _vim() {
-	echo "Installing ${bold}${red}vim${reset}..."
+	echo "Installing ${bold}${red}vim${reset} ..."
 	sudo apt install -y vim vim-gnome
 }
 
 function _vimrc() {
 	_checkfile vimrc
-	echo "Setting ${bold}${red}.vimrc${reset}..."
-    _movetoroot vimrc
+	echo "Setting ${bold}${red}.vimrc${reset} ..."
+    cp -v --backup=numbered vimrc ~/.vimrc
 	vim +PlugClean +PlugInstall +qall
 }
 
 function _tmux() {
-	echo "Installing ${bold}${red}tmux${reset}..."
+	echo "Installing ${bold}${red}tmux${reset} ..."
 	sudo apt update && sudo apt install -y tmux
 }
 
 function _tmuxconf() {
 	_checkfile tmux.conf
-	echo "Setting ${bold}${red}.tmux.conf${reset}..."
-	_movetoroot tmux.conf
+	echo "Setting ${bold}${red}.tmux.conf${reset} ..."
+	cp -v --backup=numbered tmux.conf ~/.tmux.conf
 }
 
 function _tmuxbashrc() {
@@ -123,48 +119,61 @@ function _tmuxbashrc() {
 }
 
 function _sublimetext() {
-	echo "Installing ${bold}${red}Sublime Text 3${reset}..."
+	echo "Installing ${bold}${red}Sublime Text 3${reset} ..."
 	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 	sudo apt update && sudo apt install -y sublime-text
 }
 
 function _sublimekeybindings() {
-	_checkfile sublime_keybindings.json
-	echo "Setting ${bold}${red}Sublime keybindings${reset}..."
-	mv -v sublime_keybindings.json "$HOME/.config/sublime-text-3/Packages/User/Default (Linux).sublime-keymap"
+	_checkfile sublime/'Default (Linux).sublime-keymap'
+	echo "Setting ${bold}${red}sublime keybindings${reset} ..."
+	cp -v --backup=numbered "sublime/Default (Linux).sublime-keymap" "$HOME/.config/sublime-text-3/Packages/User/"
+}
+
+function _sublimesettings() {
+	_checkfile sublime/Preferences.sublime-settings
+	echo "Setting ${bold}${red}sublime settings${reset} ..."
+	cp -v --backup=numbered "sublime/Preferences.sublime-settings" "$HOME/.config/sublime-text-3/Packages/User/"
+}
+
+function _sublimepackages() {
+    _checkfile sublime/Package Control.sublime-settings
+    echo "Setting ${bold}${red}sublime packages${reset} ..."
+    cp -v --backup=numbered "sublime/Package Control.sublime-settings" "$HOME/.config/sublime-text-3/Packages/User/"
 }
 
 function _vscode() {
-	echo "Installing ${bold}${red}VSCode${reset}..."
+	echo "Installing ${bold}${red}VSCode${reset} ..."
 	curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 	sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/ && rm -f microsoft.gpg
-	sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+	sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"' > \
+        '/etc/apt/sources.list.d/vscode.list'
 	sudo apt update && sudo apt install -y code
 }
 
 function _googlechrome() {
-	ehco "Installing ${bold}${red}Google Chrome${reset}..."
+	ehco "Installing ${bold}${red}Google Chrome${reset} ..."
 	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 	sudo dpkg -i google-chrome-stable_current_amd64.deb
 	rm -f google-chrome-stable_current_amd64.deb
 	# Remove google chrome keyring pop-up
-	sudo sed -i '/^Exec=/s/$/ --password-store=basic %U/' /usr/share/applications/google-chrome.desktop
+	sudo sed -i '/^Exec=/s/$/ --password-store=basic %U/' "/usr/share/applications/google-chrome.desktop"
 }
 
 function _neofetch() {
-	echo "Installing ${bold}${red}neofetch${reset}..."
+	echo "Installing ${bold}${red}neofetch${reset} ..."
 	sudo add-apt-repository -y ppa:dawidd0811/neofetch
 	sudo apt update && sudo apt install -y neofetch
 }
 
 function _xclip() {
-	echo "Installing ${bold}${red}xclip${reset}..."
+	echo "Installing ${bold}${red}xclip${reset} ..."
 	sudo apt update && sudo apt install -y xclip
 }
 
 function _powerline() {
-	echo "Installing ${bold}${red}powerline${reset}..."
+	echo "Installing ${bold}${red}powerline${reset} ..."
 	sudo apt install -y python-pip
 	pip install powerline-status
 	pip install powerline-gitstatus
@@ -189,31 +198,34 @@ function _powerlinebashrc() {
 function _powerlineconfig() {
 	_checkfile powerline_configs/themes/shell/default.json && _checkfile powerline_configs/colorschemes/default.json
 
-	echo "Setting ${bold}${red}themes/shell/default.json${reset}..."
-	mv -v powerline_configs/themes/shell/default.json "$HOME/.local/lib/python2.7/site-packages/powerline/config_files/themes/shell" -v --backup=numbered
+	echo "Setting ${bold}${red}themes/shell/default.json${reset} ..."
+	cp -v --backup=numbered "powerline_configs/themes/shell/default.json" \
+		"$HOME/.local/lib/python2.7/site-packages/powerline/config_files/themes/shell"
 
-	echo "Setting ${bold}${red}colorschemes/default.json${reset}..."
-	mv -v powerline_configs/colorschemes/default.json "${HOME}/.local/lib/python2.7/site-packages/powerline/config_files/colorschemes" -v --backup=numbered
+	echo "Setting ${bold}${red}colorschemes/default.json${reset} ..."
+	cp -v --backup=numbered "powerline_configs/colorschemes/default.json" \
+		"$HOME/.local/lib/python2.7/site-packages/powerline/config_files/colorschemes"
 }
 
 function _dconfsettings() {
 	_checkfile dconf/settings.dconf
-	echo "Setting ${bold}${red}dconf settings${reset}..."
+	echo "Setting ${bold}${red}dconf_settings${reset} ..."
 	dconf load / < dconf/settings.dconf
 }
 
 function _preload() {
-	echo "Installing ${bold}${red}preload${reset}..."
+	echo "Installing ${bold}${red}preload${reset} ..."
 	sudo apt update && sudo apt install -y preload
 }
 
 function _vmswappiness() {
-	echo "Changing ${bold}${red}vm.swappiness${reset} to 10..."
+	echo "Changing ${bold}${red}vm.swappiness${reset} to 10 ..."
 	echo "vm.swappiness=10" | sudo tee -a /etc/sysctl.conf
 }
 
 function _showinfo() {
-	echo "${bold}${start_underline}This script provides an easy way to install my packages and my configurations.${end_underline}${reset}"
+	echo "${bold}${start_underline}This script provides an easy way to install my packages and my configurations." \
+    "${end_underline}${reset}"
 	echo "Script is executed from: ${bold}$(pwd)${reset}"
 }
 
@@ -226,42 +238,38 @@ function _showmenu() {
 
 # ---------------------------------------------------------- Installers ------------------------------------------------------------
 function _fresh_install() {
-	_curl &&
-	(
-		_dconfsettings
-		_bashaliases
-		_preload
-		_vmswappiness
-		_xclip
-		_neofetch
-		_git && _gitconfig && _gitsofancy
-		_vim && _vimrc
-		_tmux && _tmuxconf && _tmuxbashrc
-		_powerline && _powerlineconfig && _powerlinebashrc
-		_sublimetext && _sublimekeybindings
-		_vscode
-		_googlechrome
-		_reboot
-	)
+	_curl && _git
+	_dconfsettings
+	_bashaliases
+	_preload
+	_vmswappiness
+	_xclip
+	_neofetch
+	_gitconfig && _gitsofancy
+	_vim && _vimrc
+	_tmux && _tmuxconf && _tmuxbashrc
+	_powerline && _powerlineconfig && _powerlinebashrc
+	_sublimetext && _sublimekeybindings && _sublimesettings
+	_vscode
+	_googlechrome
+	_reboot
 }
 
 function _selective_install_1b1() {
-	_curl &&
-	(
-		_prompt _dconfsettings
-		_prompt _bashaliases
-		_prompt _preload
-		_prompt _vmswappiness
-		_prompt _xclip 
-		_prompt _neofetch
-		_prompt _git && _prompt _gitconfig && _prompt _gitsofancy
-		_prompt _vim && _prompt _vimrc
-		_prompt _tmux && _prompt _tmuxconf && _prompt _tmuxbashrc
-		_prompt _powerline && _prompt _powerlineconfig && _prompt _powerlinebashrc
-		_prompt _sublimetext && _prompt _sublimekeybindings
-		_prompt _vscode
-		_prompt _googlechrome
-	)
+	_curl && _git
+	_prompt _dconfsettings
+	_prompt _bashaliases
+	_prompt _preload
+	_prompt _vmswappiness
+	_prompt _xclip 
+	_prompt _neofetch
+	_prompt _gitconfig && _prompt _gitsofancy
+	_prompt _vim && _prompt _vimrc
+	_prompt _tmux && _prompt _tmuxconf && _prompt _tmuxbashrc
+	_prompt _powerline && _prompt _powerlineconfig && _prompt _powerlinebashrc
+	_prompt _sublimetext && _prompt _sublimekeybindings && _prompt _sublimesettings
+	_prompt _vscode
+	_prompt _googlechrome
 }
 
 # -------------------------------------------------------------- Main --------------------------------------------------------------
@@ -273,6 +281,6 @@ if [[ $input -eq 1 ]]; then
 elif [[ $input -eq 2 ]]; then
 	_selective_install_1b1
 else
-	echo -e "Wrong input. Available options: [1, 2].\nExiting..."
+	echo -e "Wrong input. Available options: [1, 2].\nExiting ..."
 	exit
 fi
