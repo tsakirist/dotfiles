8c31ddf798bfa7f758cc726c1bb10c5da0e9d8b8
Tsakiris Tryfon <tsakiris@uth.gr>
2019-11-25 13:14:58 +0200

    Added separate function for the prints

--- installer.sh
+++ installer.sh
@@ -65,15 +65,27 @@ function _checkcommand() {
     fi
 }
 
+function _print() {
+    local action="Undefined"
+    if [[ $# -gt 1 ]]; then
+        case "$1" in
+            "s" ) action="Setting" ;;
+            "i" ) action="Installing" ;;
+            "c" ) action="Changing" ;;
+        esac
+        echo -e "${thunder} ${action} ${bold}${red}${*:2}${reset} ..."
+    fi
+}
+
 function _gitconfig() {
     _checkfile gitconfig
-    echo -e "${thunder} Setting ${bold}${red}.gitconfig${reset} ..."
+    _print s .gitconfig
     cp -v --backup=numbered gitconfig ~/.gitconfig
 }
 
 function _gitsofancy() {
     if ! command -v diff-so-fancy > /dev/null 2>&1; then
-        echo -e "${thunder} Setting ${bold}${red}git-diff-so-fancy${reset} ..."
+        _print s git-diff-so-fancy
         wget -q "https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy"
         chmod +x diff-so-fancy && sudo mv diff-so-fancy /usr/bin/
     fi
@@ -81,31 +93,31 @@ function _gitsofancy() {
 
 function _bashrc() {
     _checkfile bashrc
-    echo -e "${thunder} Setting ${bold}${red}.bashrc${reset} ..."
+    _print s .bashrc
     cp -v --backup=numbered bashrc ~/.bashrc
     echo -e "You should source the ${bold}${red}~/.bashrc${reset} for the changes to have effect ..."
 }
 
 function _bashaliases() {
     _checkfile bash_aliases
-    echo -e "${thunder} Setting ${bold}${red}.bash_aliases${reset} ..."
+    _print s .bash_aliases
     cp -v --backup=numbered bash_aliases ~/.bash_aliases
 }
 
 function _vim() {
-    echo -e "${thunder} Installing ${bold}${red}vim${reset} ..."
+    _print i vim
     sudo apt install -y vim vim-gnome
 }
 
 function _vimrc() {
     _checkfile vimrc
-    echo -e "${thunder} Setting ${bold}${red}.vimrc${reset} ..."
+    _print s .vimrc
     cp -v --backup=numbered vimrc ~/.vimrc
     vim +PlugInstall +qall
 }
 
 function _nvim() {
-    echo -e "${thunder} Installing ${bold}${red}neovim${reset} ..."
+    _print i neovim
     sudo sh -c 'echo "deb http://ppa.launchpad.net/neovim-ppa/stable/ubuntu bionic main" > \
                 /etc/apt/sources.list.d/neovim.list'
     sudo apt update && sudo apt install -y neovim
@@ -113,6 +125,7 @@ function _nvim() {
 
 function _nvimrc() {
     _checkfile vimrc && _checkfile init.vim
+    _print s .vimrc and init.vim
     echo -e "${thunder} Setting ${bold}${red}.vimrc and init.vim${reset} ..."
     cp -v --backup=numbered vimrc ~/.vimrc
     mkdir -v -p ~/.config/nvim/
@@ -121,18 +134,18 @@ function _nvimrc() {
 }
 
 function _tmux() {
-    echo -e "${thunder} Installing ${bold}${red}tmux${reset} ..."
+    _print i tmux
     sudo apt install -y tmux
 }
 
 function _tmuxconf() {
     _checkfile tmux.conf
-    echo -e "${thunder} Setting ${bold}${red}.tmux.conf${reset} ..."
+    _print s .tmux.conf
     cp -v --backup=numbered tmux.conf ~/.tmux.conf
 }
 
 function _sublimetext() {
-    echo -e "${thunder} Installing ${bold}${red}SublimeText 3${reset} ..."
+    _print i SublimeText 3
     wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
     sudo sh -c 'echo "deb https://download.sublimetext.com/ apt/stable/" > \
                 /etc/apt/sources.list.d/sublime-text.list'
@@ -141,25 +154,25 @@ function _sublimetext() {
 
 function _sublimesettings() {
     _checkfile sublime/Preferences.sublime-settings
-    echo -e "${thunder} Setting ${bold}${red}sublime settings${reset} ..."
+    _print s sublime settings
     cp -v --backup=numbered "sublime/Preferences.sublime-settings" "$HOME/.config/sublime-text-3/Packages/User/"
 }
 
 function _sublimekeybindings() {
     _checkfile "sublime/Default (Linux).sublime-keymap"
-    echo -e "${thunder} Setting ${bold}${red}sublime keybindings${reset} ..."
+    _print s sublime keybindings
     cp -v --backup=numbered "sublime/Default (Linux).sublime-keymap" "$HOME/.config/sublime-text-3/Packages/User/"
 }
 
 function _sublimepackages() {
     _checkfile "sublime/Package Control.sublime-settings"
-    echo -e "${thunder} Setting ${bold}${red}sublime packages${reset} ..."
+    _print s sublime packages
     cp -v --backup=numbered "sublime/Package Control.sublime-settings" "$HOME/.config/sublime-text-3/Packages/User/"
 }
 
 function _sublimeterminus() {
     _checkfile "sublime/Terminus.sublime-settings"
-    echo -e "${thunder} Setting ${bold}${red}sublime terminus settings${reset} ..."
+    _print s sublime terminus settings
     cp -v --backup=numbered "sublime/Terminus.sublime-settings" "$HOME/.config/sublime-text-3/Packages/User/"
 }
 
@@ -168,7 +181,7 @@ function _sublimeallconfigs() {
 }
 
 function _vscode() {
-    echo -e "${thunder} Installing ${bold}${red}Visual Studio Code${reset} ..."
+    _print i Visual Studio Code
     curl -s https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
     sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/ && rm -f microsoft.gpg
     sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > \
@@ -177,7 +190,7 @@ function _vscode() {
 }
 
 function _googlechrome() {
-    echo -e "${thunder} Installing ${bold}${red}Google Chrome${reset} ..."
+    _print i Google Chrome
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
     sudo dpkg -i google-chrome-stable_current_amd64.deb
     rm -f google-chrome-stable_current_amd64.deb
@@ -186,17 +199,17 @@ function _googlechrome() {
 }
 
 function _neofetch() {
-    echo -e "${thunder} Installing ${bold}${red}neofetch${reset} ..."
+    _print i neofetch
     sudo apt install -y neofetch
 }
 
 function _xclip() {
-    echo -e "${thunder} Installing ${bold}${red}xclip${reset} ..."
+    _print i xclip
     sudo apt install -y xclip
 }
 
 function _powerline() {
-    echo -e "${thunder} Installing ${bold}${red}powerline${reset} ..."
+    _print i powerline
     sudo apt install -y python-pip
     pip install powerline-status
     pip install powerline-gitstatus
@@ -206,60 +219,68 @@ function _powerline() {
 function _powerlineconfig() {
     _checkfile powerline_configs/themes/shell/default.json && _checkfile powerline_configs/colorschemes/default.json
 
-    echo -e "${thunder} Setting ${bold}${red}themes/shell/default.json${reset} ..."
+    _print s themes/shell/default.json
     cp -v --backup=numbered "powerline_configs/themes/shell/default.json" \
         "$HOME/.local/lib/python2.7/site-packages/powerline/config_files/themes/shell"
 
-    echo -e "${thunder} Setting ${bold}${red}colorschemes/default.json${reset} ..."
+    _print s colorschemes/default.json
     cp -v --backup=numbered "powerline_configs/colorschemes/default.json" \
         "$HOME/.local/lib/python2.7/site-packages/powerline/config_files/colorschemes"
 }
 
 function _dconfsettings() {
     _checkfile dconf/settings.dconf
-    echo -e "${thunder} Setting ${bold}${red}dconf_settings${reset} ..."
+    _print s dconf_settings
     dconf load / < dconf/settings.dconf
 }
 
 function _preload() {
-    echo -e "${thunder} Installing ${bold}${red}preload${reset} ..."
+    _print i preload
     sudo apt install -y preload
 }
 
 function _vmswappiness() {
-    echo -e "${thunder} Changing ${bold}${red}vm.swappiness${reset} to 10 ..."
+    _print c vm.swappiness to 10
     echo "vm.swappiness=10" | sudo tee -a /etc/sysctl.conf > /dev/null 2>&1;
 }
 
 function _cmake() {
-    echo -e "${thunder} Installing ${bold}${red}cmake${reset} ..."
+    _print i cmake
     sudo apt install -y cmake
 }
 
 function _tree() {
-    echo -e "${thunder} Installing ${bold}${red}tree${reset} ..."
+    _print i tree
     sudo apt install -y tree
 }
 
 function _htop() {
-    echo -e "${thunder} Installing ${bold}${red}htop${reset} ..."
+    _print i htop
     sudo apt install -y htop
 }
 
 function _gnometweaks() {
-    echo -e "${thunder} Installing ${bold}${red}gnome-tweaks${reset} ..."
+    _print i gnome-tweaks
     sudo apt install -y gnome-tweaks
 }
 
 function _gnomeshellextensions() {
-    echo -e "${thunder} Installing ${bold}${red}gnome-shell-extensions${reset} ..."
+    _print i gnome-shell-extensions
     sudo apt install -y gnome-shell-extension-weather gnome-shell-extension-dashtodock
 }
 
 function _java() {
-    echo -e "${thunder} Installing ${bold}${red}java and javac${reset} ..."
+    _print i java and javac
     sudo apt install -y default-jre default-jdk
 }
+
+function _zsh() {
+    _print i zsh
+    sudo apt install -y zsh
+}
+
+# function _tilix() {
+# }
 
 function _showmenu() {
     _checkcommand whiptail
