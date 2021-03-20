2ff04ec6d830fd99b5c59a69ec41a07c9e00bcae
Tsakiris Tryfon <tr.tsakiris@gmail.com>
2021-03-20 20:51:36 +0200

    clean_backups now accepts --force argument

--- clean_backups.sh
+++ clean_backups.sh
@@ -3,13 +3,24 @@
 echo -e "-- Trying to remove backups created by installer.sh. --\n"
 
 cat << EOF
-  _                                                    
- /  |  _   _. ._  o ._   _    |_   _.  _ |      ._   _ 
- \_ | (/_ (_| | | | | | (_|   |_) (_| (_ |< |_| |_) _> 
-                         _|                     |      
+  _
+ /  |  _   _. ._  o ._   _    |_   _.  _ |      ._   _
+ \_ | (/_ (_| | | | | | (_|   |_) (_| (_ |< |_| |_) _>
+                         _|                     |
 
 EOF
 
-rm -i $HOME/.*.~*
-rm -i $HOME/.config/nvim/init.vim.~*
-rm -i $HOME/.config/nvim/coc-settings.json.~*
+RM_OPT="-i"
+
+while [ -n "$1" ]; do
+    case "$1" in
+        -f | --force)
+            RM_OPT="-f"
+            ;;
+    esac
+    shift
+done
+
+rm $RM_OPT $HOME/.*.~*
+rm $RM_OPT $HOME/.config/nvim/init.vim.~*
+rm $RM_OPT $HOME/.config/nvim/coc-settings.json.~*
