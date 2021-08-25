9660450dc6f79883d581030ad68d806ad6fa1330
Tsakiris Tryfon <tr.tsakiris@gmail.com>
2021-08-25 22:55:58 +0300

    Updated vscode installation

--- installer.sh
+++ installer.sh
@@ -249,8 +249,7 @@ function _tmux_config() {
 
 function _vscode() {
     _print i "Visual Studio Code"
-    curl -s https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-    sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/ && rm -f microsoft.gpg
+    wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add - > /dev/null 2>&1
     sudo sh -c "echo 'deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main' > \
                 /etc/apt/sources.list.d/vscode.list"
     sudo apt-get -qq update && _install code
