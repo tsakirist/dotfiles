f798e5eb81ab2130ab8ac083e3439919c252b86c
Tsakiris Tryfon <tr.tsakiris@gmail.com>
2021-12-31 13:58:16 +0200

    Added icons to alpha

--- nvim/lua/tt/plugins/_alpha.lua
+++ nvim/lua/tt/plugins/_alpha.lua
@@ -14,9 +14,9 @@ startify.section.header.val = {
     [[   \ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
     [[    \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
     [[                                                    ]],
-    [[     Tryfon Tsakiris, tr.tsakiris@gmail.com         ]],
-    "     " .. datetime,
-    "     Neovim Version: " .. version,
+    [[   ﯟ Tryfon Tsakiris, tr.tsakiris@gmail.com         ]],
+    [[    ]] .. datetime,
+    [[    Neovim Version: ]] .. version,
 }
 
 startify.section.top_buttons.val = {
@@ -33,7 +33,7 @@ startify.section.bottom_buttons.val = {
     startify.file_button(path .. "/lua/tt/plugins.lua", "p"),
     startify.file_button(path .. "/init.lua", "i"),
     { type = "padding", val = 2 },
-    startify.button("q", "  Quit", ":qa<CR>"),
+    startify.button("q", "  Quit", ":qa<CR>"),
 }
 
 startify.opts = {
@@ -53,3 +53,54 @@ startify.opts = {
 }
 
 alpha.setup(require("alpha.themes.startify").opts)
+
+-- Set header
+-- dashboard.section.header.val = {
+--     "                                                     ",
+--     "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+--     "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+--     "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+--     "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+--     "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+--     "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+--     "                                                     ",
+-- }
+
+-- -- Set menu
+-- dashboard.section.buttons.val = {
+--     dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
+--     dashboard.button("f", "  > Find file", ":cd $HOME/Workspace | Telescope find_files<CR>"),
+--     dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
+--     dashboard.button("s", "  > Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+--     dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
+-- }
+
+-- -- Plugins
+-- -- local total_plugins = #vim.tbl_keys(packer_plugins)
+-- -- local loaded_plugins = #vim.fn.globpath(vim.fn.stdpath "data" .. "/site/pack/packer/start", "*", 0, 1)
+-- -- local plugins_text = "Total plugins: " .. total_plugins .. "\nLoaded plugins: " .. loaded_plugins
+
+-- -- Date
+-- -- local datetime = os.date "%d-%m-%Y  %H:%M:%S"
+-- --
+-- local function get_footer()
+--     local total_plugins = #vim.tbl_keys(packer_plugins)
+--     local loaded_plugins = #vim.fn.globpath(vim.fn.stdpath "data" .. "/site/pack/packer/start", "*", 0, 1)
+--     local datetime = os.date "%A %d %B %Y, %T"
+
+--     local footer = {
+--         "Plugins (L/T): (" .. loaded_plugins .. "/" .. total_plugins .. ")",
+--         "",
+--         datetime,
+--     }
+
+--     return quote
+-- end
+
+-- -- Set footer
+-- -- local fortune = require "alpha.fortune"
+-- -- dashboard.section.footer.val = fortune()
+-- dashboard.section.footer.val = get_footer()
+
+-- -- Send config to alpha
+-- alpha.setup(dashboard.opts)
