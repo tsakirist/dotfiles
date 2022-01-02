5fff7cfa10227a307f1a3d8087d657443a4242b8
Tsakiris Tryfon <tr.tsakiris@gmail.com>
2022-01-02 21:47:14 +0200

    Updated gitsigns keymaps and added more comments

--- nvim/lua/tt/plugins/gitsigns.lua
+++ nvim/lua/tt/plugins/gitsigns.lua
@@ -38,25 +38,26 @@ function M.setup()
         numhl = true, -- Highlights just the number part of the number column
         linehl = false, -- Highlights the whole line
         word_diff = false, -- Highlights just the part of the line that has changed
-        watch_gitdir = {
-            interval = 1000,
-            follow_files = true,
+        watch_gitdir = { -- Add a watcher for the .git directory to detect changes
+            interval = 1000, -- Interval to wait before polling gitdir in (ms)
+            follow_files = true, -- Follow files e.g. git mv
         },
-        attach_to_untracked = true,
-        current_line_blame = false,
-        current_line_blame_opts = {
-            virt_text = true,
+        attach_to_untracked = true, -- Attach to untracked files
+        current_line_blame = false, -- Add current line blame
+        current_line_blame_opts = { -- Options for lineblame
+            virt_text = true, -- Whether to show virtual_text for line blame
             virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-            delay = 1000,
+            delay = 1000, -- Delay before line blame is shown
+            ignore_whitespace = false, -- Ignore whitespaces
         },
-        current_line_blame_formatter_opts = {
+        current_line_blame_formatter_opts = { -- Options for line-blame formatter
             relative_time = false,
         },
-        sign_priority = 6,
-        update_debounce = 200,
-        status_formatter = nil,
-        max_file_length = 40000,
-        preview_config = { -- Options passed to nvim_open_win
+        sign_priority = 6, -- Sign priority
+        update_debounce = 200, -- Debounce time for updates in (ms)
+        status_formatter = nil, -- Use default status_formatter
+        max_file_length = 40000, -- Max file length to attach to
+        preview_config = { -- Options to pass to nvim_open_win
             border = "rounded",
             style = "minimal",
             relative = "cursor",
@@ -64,30 +65,30 @@ function M.setup()
             col = 1,
         },
         keymaps = {
-            -- Default keymap options
             noremap = true,
             buffer = true,
 
             -- Move between hunks
             ["n <leader>gj"] = {
                 expr = true,
-                "&diff ? ']c' : '<Cmd>lua require\"gitsigns.actions\".next_hunk()<CR>'",
+                "&diff ? ']c' : '<Cmd>Gitsigns next_hunk<CR>'",
             },
             ["n <leader>gk"] = {
                 expr = true,
-                "&diff ? '[c' : '<Cmd>lua require\"gitsigns.actions\".prev_hunk()<CR>'",
+                "&diff ? '[c' : '<Cmd>Gitsigns prev_hunk<CR>'",
             },
 
             -- Hunk specific
-            ["n <leader>hs"] = "<Cmd>lua require'gitsigns'.stage_hunk()<CR>",
-            ["v <leader>hs"] = "<Cmd>lua require'gitsigns'.stage_hunk({vim.fn.line('.'), vim.fn.line('v')})<CR>",
-            ["n <leader>hu"] = "<Cmd>lua require'gitsigns'.undo_stage_hunk()<CR>",
-            ["n <leader>hr"] = "<Cmd>lua require'gitsigns'.reset_hunk()<CR>",
-            ["v <leader>hr"] = "<Cmd>lua require'gitsigns'.reset_hunk({vim.fn.line('.'), vim.fn.line('v')})<CR>",
-            ["n <leader>hp"] = "<Cmd>lua require'gitsigns'.preview_hunk()<CR>",
-            ["n <leader>hb"] = "<Cmd>lua require'gitsigns'.blame_line{full=true}<CR>",
-            ["n <leader>hS"] = "<Cmd>lua require'gitsigns'.stage_buffer()<CR>",
-            ["n <leader>hU"] = "<Cmd>lua require'gitsigns'.reset_buffer_index()<CR>",
+            ["n <leader>hs"] = "<Cmd>Gitsigns stage_hunk<CR>",
+            ["v <leader>hs"] = ":Gitsigns stage_hunk<CR>",
+            ["n <leader>hu"] = "<Cmd>Gitsigns undo_stage_hunk<CR>",
+            ["n <leader>hr"] = "<Cmd>Gitsigns reset_hunk<CR>",
+            ["v <leader>hr"] = ":Gitsigns reset_hunk<CR>",
+            ["n <leader>hR"] = "<Cmd>Gitsigns reset_buffer<CR>",
+            ["n <leader>hp"] = "<Cmd>Gitsigns preview_hunk<CR>",
+            ["n <leader>hb"] = "<Cmd>Gitsigns blame_line{full=true}<CR>",
+            ["n <leader>hS"] = "<Cmd>Gitsigns stage_buffer<CR>",
+            ["n <leader>hU"] = "<Cmd>Gitsigns reset_buffer_index<CR>",
 
             -- Toggling options
             ["n <leader>gb"] = "<Cmd>Gitsigns toggle_current_line_blame<CR>",
@@ -97,8 +98,8 @@ function M.setup()
             ["n <leader>gw"] = "<Cmd>Gitsigns toggle_word_diff<CR>",
 
             -- Text objects
-            ["o ih"] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
-            ["x ih"] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
+            ["o ih"] = ":<C-U>Gitsigns select_hunk()<CR>",
+            ["x ih"] = ":<C-U>Gitsigns select_hunk()<CR>",
         },
     }
 end
