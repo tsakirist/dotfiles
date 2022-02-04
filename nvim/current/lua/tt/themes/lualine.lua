790e0b7a4590ad12e900924673ec3c4b32c4de05
Tsakiris Tryfon <tr.tsakiris@gmail.com>
2022-02-04 13:03:49 -0800

    Added some more configs for lualine

--- nvim/current/lua/tt/themes/lualine.lua
+++ nvim/current/lua/tt/themes/lualine.lua
@@ -4,18 +4,44 @@ function M.setup()
     -- Module that shows the current cursor context
     local gps = require "nvim-gps"
 
+    -- Get the colors of the theme
+    local colors = require("nightfox.colors").load "nordfox"
+
     require("lualine").setup {
         options = {
-            theme = "nightfox",
             icons_enabled = true,
+            always_divide_middle = true,
+            theme = "nightfox",
+            component_separators = { left = "", right = "" },
+            section_separators = { left = "", right = "" },
         },
         sections = {
+            lualine_a = {
+                {
+                    "mode",
+                    padding = 1,
+                    separator = { right = " " },
+                    icon = "",
+                },
+            },
+            lualine_b = {
+                "branch",
+                "diff",
+                {
+                    "diagnostics",
+                    symbols = { error = "", hint = "", info = "", warn = "" },
+                },
+            },
             lualine_c = {
                 {
                     gps.get_location,
                     cond = gps.is_available,
+                    color = { fg = colors.magenta },
                 },
             },
+            lualine_x = { "encoding", "fileformat", "filetype" },
+            lualine_y = { "progress" },
+            lualine_z = { "location" },
         },
     }
 end
