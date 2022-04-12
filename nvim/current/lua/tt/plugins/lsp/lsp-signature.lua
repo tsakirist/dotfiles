75fb972f6b9b3915b8c5e112a0c0881a0b189574
Tsakiris Tryfon <tr.tsakiris@gmail.com>
2022-04-12 05:02:30 -0700

    Made LSP-signature to not pop-up by default

--- nvim/current/lua/tt/plugins/lsp/lsp-signature.lua
+++ nvim/current/lua/tt/plugins/lsp/lsp-signature.lua
@@ -7,18 +7,20 @@ function M.setup()
         handler_opts = { -- Options for floating window
             border = "rounded",
         },
-        floating_window = true, -- Show hint in a floating window, set to false for virtual text only mode
+        floating_window = false, -- Show hint in a floating window. Setting it to false in order to manually trigger it
         floating_window_above_cur_line = true, -- Try to place the floating above the current line when possible
         fix_pos = false, -- Set to true, the floating window will not auto-close until all parameters finished
-        hint_enable = false, -- Virtual hint enable
+        hint_enable = false, -- Virtual hint
         hint_prefix = " ", -- Show a prefix before the virtual text
         hint_scheme = "String", -- Hint's scheme
         hi_parameter = "Search", -- Parameter highlight
-        toggle_key = "<M-x>", -- Toggle signature on or off in insert mode
         padding = "", -- Character to pad on left and right of signature can be ' ', or '|' etc
         extra_trigger_chars = { -- Array of extra characters that will trigger signature completion
-            '{',
+            "{",
         },
+        always_trigger = false, -- Sometimes showing signature on a new line or in  the middle of parameter can be confusing
+        auto_close_after = nil, -- Autoclose signature float win after x sec, disabled if nil
+        toggle_key = "<M-x>", -- Toggle signature on or off in insert mode
     }
 end
 
