1a2ab78a6f762a62ba97106189a1dd3dc5fe9616
Tsakiris Tryfon <tr.tsakiris@gmail.com>
2022-05-25 20:35:30 +0300

    Add easier quit for some filetypes

--- nvim/current/lua/tt/autocommands.lua
+++ nvim/current/lua/tt/autocommands.lua
@@ -21,52 +21,52 @@ end
 
 function M.load_autocommands()
     local definitions = {
-        -- Format options configuration:
-        -- 't': is required in format options to wrap text in insert mode
-        -- 'l': a line that is longer than textwidth may not be wraped if 'l' is in format options
-        -- 'r': makes <CR> key to autocomment when pressing enter in line that contains a comment, enables javadoc
-        -- 'o': insert comment when pressing 'o' or 'O'
-        -- 'q': allow formatting comments with gq
+        --- Format options configuration:
+        --- 't': is required in format options to wrap text in insert mode
+        --- 'l': a line that is longer than textwidth may not be wraped if 'l' is in format options
+        --- 'r': makes <CR> key to autocomment when pressing enter in line that contains a comment, enables javadoc
+        --- 'o': insert comment when pressing 'o' or 'O'
+        --- 'q': allow formatting comments with gq
         _format_options = {
             { "BufEnter", "*", "setlocal fo+=t fo+=r fo-=l fo-=o fo+=q" },
         },
-        -- Remove trailing whitespaces on write
+        --- Remove trailing whitespaces on write
         _trim_whitespace = {
             { "BufWritePre", "*", "lua require('tt.helper').trimTrailingWhiteSpace()" },
         },
-        -- Enable format on save
+        --- Enable format on save
         _format_on_save = {
             { "BufWritePre", "*.lua,*.tsx,*.ts,*.sh", "lua vim.lsp.buf.format()" },
         },
-        -- Enable spelling for these filetypes
+        --- Enable spelling for these filetypes
         _spell_check = {
             { "FileType", "text,gitcommit,markdown", "setlocal spell wrap" },
         },
-        -- Resize windows when host resizes
+        --- Resize windows when host resizes
         _auto_resize = {
             { "VimResized", "*", "tabdo wincmd =" },
         },
-        -- Enable highlighting when yanking text
+        --- Enable highlighting when yanking text
         _highlight = {
             { "TextYankPost", "*", "silent! lua vim.highlight.on_yank()" },
         },
-        -- Enter insert mode and remove line numbers when in terminal
+        --- Enter insert mode and remove line numbers when in terminal
         _terminal = {
             { "TermOpen,BufEnter,WinEnter", "*", "if &buftype == 'terminal' | :startinsert | endif" },
             { "TermOpen", "*", "setlocal nonumber norelativenumber" },
         },
-        -- Automatic toggling between hybrid and absolute line numbers
+        --- Automatic toggling between hybrid and absolute line numbers
         _number_toggle = {
             { "BufEnter,FocusGained,InsertLeave,WinEnter", "*", "if &nu && mode() != 'i' | set rnu | endif" },
             { "BufLeave,FocusLost,InsertEnter,WinLeave", "*", "if &nu | set nornu | endif" },
         },
-        -- Regenerate compiled loader file everytime the plugins file changes
+        --- Regenerate compiled loader file everytime the plugins file changes
         _packer = {
             { "BufWritePost", "plugins.lua", "source <afile> | PackerCompile" },
         },
-        -- Close man page inside neovim with 'q', to mimic the default behavior, since the default keybind don't work
-        _manpager = {
-            { "FileType", "man", "nnoremap <silent> <buffer> q :quit<CR>" },
+        --- A set of filetypes where just hitting q should exit the window
+        _faster_quit = {
+            { "FileType", "help,man,lspinfo", "nnoremap <silent> <buffer> q :quit<CR>" },
         },
     }
     M.create_augroups(definitions)
