b32da9142ff736fca00b59e8c9c413deace8ebbe
Tsakiris Tryfon <tr.tsakiris@gmail.com>
2022-07-06 22:20:39 +0300

    Fix: check for  too for the IsWSL() check

--- nvim/current/lua/tt/globals.lua
+++ nvim/current/lua/tt/globals.lua
@@ -40,5 +40,5 @@ end
 ---@return boolean
 function _G.IsWSL()
     local output = vim.fn.systemlist "uname -r"
-    return not not string.find(output[1] or "", "WSL")
+    return not not string.find(output[1] or "", "WSL") or string.find(output[1] or "", "Microsoft")
 end
