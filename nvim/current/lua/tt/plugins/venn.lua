be89862fb942562b7a75a5a9e0e25d1fb2647244
Tsakiris Tryfon <tr.tsakiris@gmail.com>
2022-07-29 21:37:47 +0300

    Venn replace notification

--- nvim/current/lua/tt/plugins/venn.lua
+++ nvim/current/lua/tt/plugins/venn.lua
@@ -3,18 +3,19 @@ local M = {}
 function M.setup()
     local utils = require "tt.utils"
 
+    -- Hold the instance of the last notification for replacement
+    local venn_notification = nil
+
     local function venn_toggle()
         local title = "Venn"
         if vim.b.venn_enabled == nil then
             vim.b.venn_enabled = true
             vim.cmd [[setlocal virtualedit=all]]
 
-            -- Draw a line on HJKL keystokes
             utils.map("n", "J", "<C-v>j:VBox<CR>", { buffer = true })
             utils.map("n", "K", "<C-v>k:VBox<CR>", { buffer = true })
             utils.map("n", "L", "<C-v>l:VBox<CR>", { buffer = true })
             utils.map("n", "H", "<C-v>h:VBox<CR>", { buffer = true })
-            -- Draw a box by pressing "B" with visual selection
             utils.map("v", "B", ":VBox<CR>", { buffer = true })
         else
             vim.b.venn_enabled = nil
@@ -22,9 +23,14 @@ function M.setup()
             vim.cmd [[mapclear <buffer>]]
         end
 
-        vim.notify(("Venn has been %s!"):format(vim.b.venn_enabled and "enabled" or "disabled"), vim.log.levels.INFO, {
-            title = title,
-        })
+        venn_notification = vim.notify(
+            ("Venn has been %s!"):format(vim.b.venn_enabled and "enabled" or "disabled"),
+            vim.log.levels.INFO,
+            {
+                title = title,
+                replace = venn_notification,
+            }
+        )
     end
 
     utils.map("n", "<leader>vt", venn_toggle)
