3d8093b6cd15e3de7875f82b7dfce362d40d4e97
Tsakiris Tryfon <tr.tsakiris@gmail.com>
2022-08-02 18:06:19 +0300

    Cybu more options

--- nvim/current/lua/tt/plugins/cybu.lua
+++ nvim/current/lua/tt/plugins/cybu.lua
@@ -3,6 +3,11 @@ local M = {}
 function M.setup()
     require("cybu").setup {
         display_time = 500,
+        position = {
+            anchor = "center", -- Location of where the popup will open
+            relative_to = "editor", -- Popup will open relative to either `win`, `cursor`, or `editor`
+            max_win_height = 10, -- Number of lines to show
+        },
         style = {
             highlights = {
                 current_buffer = "CybuCurrentSelection",
