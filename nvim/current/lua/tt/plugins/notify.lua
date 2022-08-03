c874b237edabc8d8576f0f4c90245705ff4d598c
Tsakiris Tryfon <tr.tsakiris@gmail.com>
2022-08-03 21:39:30 +0300

    Remove notify highlights use default

--- nvim/current/lua/tt/plugins/notify.lua
+++ nvim/current/lua/tt/plugins/notify.lua
@@ -25,12 +25,6 @@ function M.setup()
         end,
     }
 
-    --- Set custom highlight groups for notifications
-    -- local colors = require("tt.plugins.nightfox").colors()
-    -- vim.api.nvim_set_hl(0, "NotifyINFOBorder", { fg = colors.blue.base })
-    -- vim.api.nvim_set_hl(0, "NotifyINFOIcon", { fg = colors.blue.base })
-    -- vim.api.nvim_set_hl(0, "NotifyINFOTitle", { fg = colors.blue.base })
-
     --- Set nvim-notify as the default handler for notifications
     vim.notify = require "notify"
 end
