bef0a883ba1f2d93c1051ab7c798f3710e59b8bb
Tsakiris Tryfon <tr.tsakiris@gmail.com>
2022-08-07 20:16:16 +0300

    Add window-management hydra and preview true to hunks hydra

--- nvim/current/lua/tt/plugins/hydra.lua
+++ nvim/current/lua/tt/plugins/hydra.lua
@@ -40,6 +40,73 @@ local function setup_venn_hydra()
     }
 end
 
+local function setup_window_management_hydra()
+    local hint = [[
+^  ^^^^^^              ^^^^^^ ^ ^^^^ ^          ^^^ ^ ^ ^                ^
+^  ^^^^^^     Move     ^^^^^^ ^ ^^^^  Resize   ^^^^ ^ ^     Split     ^  ^
+^  ^^^^^^--------------^^^^^^ ^ ^^^^-----------^^^^ ^ ^---------------^  ^
+^  ^ ^ _k_ ^ ^    ^ ^ _K_ ^ ^ ^ ^   ^ ^ _+_ ^ ^   ^ ^ _s_: horizontally  ^
+^  _h_ ^ ^ _l_    _H_ ^ ^ _L_ ^ ^   _<_ ^ ^ _>_   ^ ^ _v_: vertically    ^
+^  ^ ^ _j_ ^ ^    ^ ^ _J_ ^ ^ ^ ^   ^ ^ _-_ ^ ^   ^ ^ _q_: close         ^
+^  ^^^^^^cursor  window^^^^^^ ^ ^^^_=_: equalize^^^ ^ _o_: only          ^
+]]
+
+    Hydra {
+        name = "Window Management",
+        hint = hint,
+        config = {
+            invoke_on_body = true,
+            hint = {
+                border = "rounded",
+                offset = -1,
+            },
+        },
+        mode = "n",
+        body = "<leader>w",
+        heads = {
+            -- Move cursor
+            { "h", "<C-w>h" },
+            { "j", "<C-w>j" },
+            { "k", "<C-w>k" },
+            { "l", "<C-w>l" },
+            { "w", "<C-w>w", { exit = true, desc = false } },
+            { "<C-w>", "<C-w>w", { exit = true, desc = false } },
+            { "W", "<C-w>W", { exit = true, desc = false } },
+
+            -- Move window
+            { "H", "<C-w>H" },
+            { "J", "<C-w>J" },
+            { "K", "<C-w>K" },
+            { "L", "<C-w>L" },
+
+            -- Resize
+            { "+", "<C-w>+" },
+            { "-", "<C-w>-" },
+            { "<", "<C-w><" },
+            { ">", "<C-w>>" },
+            { "=", "<C-w>=", { desc = "equalize" } },
+
+            -- Split current
+            { "s", "<C-w>s" },
+            { "<C-s>", "<C-w><C-s>", { desc = false } },
+            { "v", "<C-w>v" },
+            { "<C-v>", "<C-w><C-v>", { desc = false } },
+            { "x", "<C-w>s", { desc = false } },
+
+            -- Split into tab
+            { "T", "<C-w>T", { desc = false } },
+
+            -- Close current
+            { "q", "<C-w>q", { desc = "close" } },
+            { "<C-q>", "<C-w>q", { desc = false } },
+
+            -- Close others
+            { "o", "<C-w>o", { exit = true, desc = "only" } },
+            { "<C-o>", "<C-w>o", { exit = true, desc = false } },
+        },
+    }
+end
+
 local function setup_git_hydra()
     local gitsigns = require "gitsigns"
 
@@ -88,7 +155,7 @@ local function setup_git_hydra()
                         return "]c"
                     end
                     vim.schedule(function()
-                        gitsigns.next_hunk()
+                        gitsigns.next_hunk { preview = true }
                     end)
                     return "<Ignore>"
                 end,
@@ -101,7 +168,7 @@ local function setup_git_hydra()
                         return "[c"
                     end
                     vim.schedule(function()
-                        gitsigns.prev_hunk()
+                        gitsigns.prev_hunk { preview = true }
                     end)
                     return "<Ignore>"
                 end,
@@ -130,6 +197,7 @@ end
 
 function M.setup()
     setup_venn_hydra()
+    setup_window_management_hydra()
     setup_git_hydra()
 end
 
