66d3847eb5b71f9dec3a04ce2730ce53588432d7
Tsakiris Tryfon <tr.tsakiris@gmail.com>
2022-11-16 20:07:08 +0200

    Make dressing floating-windows use default text-color

--- nvim/current/lua/tt/plugins/dressing.lua
+++ nvim/current/lua/tt/plugins/dressing.lua
@@ -7,7 +7,7 @@ function M.setup()
             prompt_align = "center",
 
             -- Highlights: 'NormalFloat' for the text, 'FloatBorder' for the border
-            winhighlight = "NormalFloat:DressingInput,FloatBorder:DressingBorder",
+            winhighlight = "FloatBorder:DressingBorder",
 
             -- Make ui.input centered by default
             relative = "editor",
