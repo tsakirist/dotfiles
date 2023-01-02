bffc7d3826f6f2287f3e45926327cd53092771c1
Tsakiris Tryfon <tr.tsakiris@gmail.com>
2023-01-02 22:16:42 +0200

    Add function to open hover on new window
    This is handy for when a symbol has long documentation and is convenient
    to open it on a separate window.

--- nvim/current/lua/tt/plugins/lsp/nvim-lspconfig.lua
+++ nvim/current/lua/tt/plugins/lsp/nvim-lspconfig.lua
@@ -143,10 +143,49 @@ local function setup_formatting(client)
 end
 
 local function setup_keymappings(_, bufnr)
+    local function hover_on_new_window()
+        vim.lsp.buf_request(
+            0,
+            "textDocument/hover",
+            vim.lsp.util.make_position_params(),
+            function(_, result, ctx, config)
+                config = config or {}
+                config.focus_id = ctx.method
+
+                -- Ignore result since buffer changed. This happens for slow language servers
+                if vim.api.nvim_get_current_buf() ~= ctx.bufnr then
+                    return
+                end
+
+                -- No information available
+                if not (result and result.contents) then
+                    return
+                end
+
+                local markdown_lines =
+                    vim.lsp.util.trim_empty_lines(vim.lsp.util.convert_input_to_markdown_lines(result.contents))
+
+                -- No information available
+                if vim.tbl_isempty(markdown_lines) then
+                    return
+                end
+
+                -- Open an new window with the hover information
+                vim.cmd.new()
+                vim.api.nvim_buf_set_option(0, "filetype", "markdown")
+                vim.api.nvim_buf_set_option(0, "buftype", "nofile")
+                vim.api.nvim_buf_set_option(0, "buflisted", false)
+                vim.api.nvim_buf_set_lines(0, 0, -1, false, markdown_lines)
+                utils.map("n", "q", "<C-w>c", { buffer = true })
+            end
+        )
+    end
+
     local opts = { buffer = bufnr }
     utils.map("n", "<C-LeftMouse>", "<Cmd>TroubleToggle lsp_references<CR>", opts)
     utils.map("n", "<leader>ca", "<Cmd>CodeActionMenu<CR>", opts)
     utils.map("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+    utils.map("n", "<leader>K", hover_on_new_window, opts)
     utils.map("n", "[d", "<Cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
     utils.map("n", "]d", "<Cmd>lua vim.diagnostic.goto_next()<CR>", opts)
     utils.map("n", "dl", "<Cmd>lua vim.diagnostic.setloclist()<CR>", opts)
