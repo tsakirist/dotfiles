0e3f8727339aa9241952b0d5cddabfca3dbf0e08
Tsakiris Tryfon <tr.tsakiris@gmail.com>
2023-02-11 18:54:40 +0200

    Make mason check for outdated versions on open

--- nvim/lua/tt/_plugins/lsp/mason.lua
+++ nvim/lua/tt/_plugins/lsp/mason.lua
@@ -19,7 +19,7 @@ function M.setup()
 
         ui = {
             -- Whether to automatically check for new versions when opening the :Mason window
-            check_outdated_packages_on_open = false,
+            check_outdated_packages_on_open = true,
 
             -- The border to use for the UI window. Accepts same border values as |nvim_open_win()|
             border = "none",
