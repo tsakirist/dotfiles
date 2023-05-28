f7ea90f82987f945115806fe959b2c6757ef158e
Tsakiris Tryfon <tr.tsakiris@gmail.com>
2023-05-28 14:43:57 +0300

    Update Lspsaga keymappings

--- nvim/lua/tt/_plugins/lsp/lsp-saga.lua
+++ nvim/lua/tt/_plugins/lsp/lsp-saga.lua
@@ -10,18 +10,44 @@ function M.setup()
         lightbulb = {
             virtual_text = false,
         },
-        code_action = {
-            extend_gitsigns = false,
-        },
-        defintion = {
-            split = "<C-c>x",
-        },
         diagnostic = {
             on_insert = false, -- Remove diagnostics on insert mode at the top right winbar
             text_hl_follow = true,
         },
+        code_action = {
+            extend_gitsigns = false,
+            keys = {
+                quit = { "<Esc> ", "q" },
+                exec = "<CR>",
+            },
+        },
+        definition = {
+            edit = { "<C-o>", "<CR>" },
+            split = "<C-x>",
+            tabe = "<C-c>t",
+            vsplit = "<C-v>",
+            quit = { "<Esc>", "q" },
+        },
         finder = {
             max_height = 0.7,
+            keys = {
+                split = "<C-x>",
+                tabnew = "<C-t>",
+                vsplit = "<C-v>",
+                expand_or_jump = { "<CR>", "o" },
+                quit = { "<Esc>", "q" },
+            },
+        },
+        callhierarchy = {
+            keys = {
+                edit = "<C-o>",
+                vsplit = "<C-v>",
+                split = "<C-x>",
+                tabe = "<C-t>",
+                jump = { "<CR>", "o" },
+                quit = { "<Esc>", "q" },
+                expand_collapse = "u",
+            },
         },
         ui = {
             title = true,
@@ -42,7 +68,7 @@ end
 function M.on_attach(_, bufnr)
     -- stylua: ignore start
     local utils = require "tt.utils"
-    utils.map("n", "db", "<Cmd>Lspsaga show_buf_diagnostics<CR>", { buffer = bufnr, desc = "Show buffer diagnostics" })
+    utils.map("n", "da", "<Cmd>Lspsaga show_buf_diagnostics<CR>", { buffer = bufnr, desc = "Show buffer diagnostics" })
     utils.map("n", "do", "<Cmd>Lspsaga show_cursor_diagnostics<CR>", { buffer = bufnr, desc = "Show cursor diagnostics" })
     utils.map("n", "dn", "<Cmd>Lspsaga diagnostic_jump_next<CR>", { buffer = bufnr, desc = "Jump to next diagnostic" })
     utils.map("n", "dp", "<Cmd>Lspsaga diagnostic_jump_prev<CR>", { buffer = bufnr, desc = "Jump to previous diagnostic" })
@@ -50,8 +76,9 @@ function M.on_attach(_, bufnr)
     utils.map("n", "gr", "<Cmd>Lspsaga lsp_finder<CR>", { buffer = bufnr, desc = "Find definition and references of current symbol" })
     utils.map("n", "K", "<Cmd>Lspsaga hover_doc<CR>", { buffer = bufnr, desc = "Show information of current symbol" })
     utils.map("n", "<leader>ca", "<Cmd>Lspsaga code_action<CR>", { buffer = bufnr, desc = "Open code action menu" })
-    utils.map("n", "<leader>ic", "<Cmd>Lspsaga incoming_calls<CR>", { buffer = bufnr, desc = "Show incoming calls" })
-    utils.map("n", "<leader>oc", "<Cmd>Lspsaga outgoing_calls<CR>", { buffer = bufnr, desc = "Show outgoing calls" })
+    utils.map("n", "<leader>IC", "<Cmd>Lspsaga incoming_calls<CR>", { buffer = bufnr, desc = "Show incoming calls" })
+    utils.map("n", "<leader>OC", "<Cmd>Lspsaga outgoing_calls<CR>", { buffer = bufnr, desc = "Show outgoing calls" })
+    utils.map("n", "<leader>OL", "<Cmd>Lspsaga outline<CR>", { buffer = bufnr, desc = "Show outline" })
 end
 
 return M
