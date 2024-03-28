12efb124eded6175b1b2ba8a5b9e1da8ac1e8873
Tsakiris Tryfon <tr.tsakiris@gmail.com>
2024-03-29 01:13:22 +0200

    Make `utils.map` to be able to take a table for LHS

--- nvim/lua/tt/utils.lua
+++ nvim/lua/tt/utils.lua
@@ -1,16 +1,21 @@
 local M = {}
 
---- Creates a new mapping
----@param mode string|table: can be for example 'n', 'i', 'v' or { 'n', 'i', 'v' }
----@param lhs string: the left hand side
+--- Creates new mapping(s).
+---@param mode string|string[]: can be for example 'n', 'i', 'v' or { 'n', 'i', 'v' }
+---@param lhs string|string[]: the left hand side
 ---@param rhs string|function: the right hand side
----@param opts table: a table containing the mapping's options e.g. silent, remap
+---@param opts table?: a table containing the mapping's options e.g. silent, remap
 function M.map(mode, lhs, rhs, opts)
     local options = { silent = true }
     if opts then
         options = vim.tbl_extend("force", options, opts)
     end
-    vim.keymap.set(mode, lhs, rhs, options)
+    if type(lhs) == "string" then
+        lhs = { lhs }
+    end
+    for _, key in ipairs(lhs) do
+        vim.keymap.set(mode, key, rhs, options)
+    end
 end
 
 --- Returns the appropriate path separator according to the current OS.
