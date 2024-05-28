eb72adabe2b7f663147e415b35977434653d37d7
Tsakiris Tryfon <tr.tsakiris@gmail.com>
2024-05-28 23:10:11 +0300

    Add `mini.ai` for extra text objects
    Still need to learn how to fully utilize its capabilities.

--- nvim/lua/tt/plugins.lua
+++ nvim/lua/tt/plugins.lua
@@ -419,6 +419,35 @@ return {
         end,
     },
 
+    -- Add extra text objects
+    {
+        "echasnovski/mini.ai",
+        event = "VeryLazy",
+        opts = function()
+            local ai = require "mini.ai"
+            return {
+                n_lines = 500,
+                custom_textobjects = {
+                    o = ai.gen_spec.treesitter {
+                        a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+                        i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+                    },
+                    f = ai.gen_spec.treesitter { a = "@function.outer", i = "@function.inner" },
+                    c = ai.gen_spec.treesitter { a = "@class.outer", i = "@class.inner" },
+                    d = { "%f[%d]%d+" }, -- Digits
+                    g = function() -- Whole file
+                        local from = { line = 1, col = 1 }
+                        local to = {
+                            line = vim.fn.line "$",
+                            col = math.max(vim.fn.getline("$"):len(), 1),
+                        }
+                        return { from = from, to = to }
+                    end,
+                },
+            }
+        end,
+    },
+
     -- Visualize and operate on indent scope
     {
         "echasnovski/mini.indentscope",
