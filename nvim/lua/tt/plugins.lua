35c47e70657ceb34c5e0b6d9ec9a8627c2e421a9
Tsakiris Tryfon <tr.tsakiris@gmail.com>
2024-07-24 18:44:36 +0300

    Use width `block` with padding for `markdown`

--- nvim/lua/tt/plugins.lua
+++ nvim/lua/tt/plugins.lua
@@ -782,7 +782,13 @@ return {
             "nvim-treesitter/nvim-treesitter",
             "nvim-tree/nvim-web-devicons",
         },
-        opts = {},
+        opts = {
+            code = {
+                width = "block",
+                right_pad = 1,
+                left_pad = 1,
+            },
+        },
     },
 
     -- Nice looking animation for the current buffer
