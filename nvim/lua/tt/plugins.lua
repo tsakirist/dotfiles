de1323c3ba27bc5712a9d8993b28cc851fed7214
Tsakiris Tryfon <tr.tsakiris@gmail.com>
2024-09-10 20:46:23 +0300

    Update `egrepify` upstream after PR got merged

--- nvim/lua/tt/plugins.lua
+++ nvim/lua/tt/plugins.lua
@@ -382,7 +382,7 @@ return {
                 build = vim.fn.executable "make" == 1 and "make" or "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && \
                     cmake --build build --config Release  &&  cmake --install build --prefix build",
             },
-            { "tsakirist/telescope-egrepify.nvim", branch = "fix-devicons" },
+            { "fdschmidt93/telescope-egrepify.nvim" },
             { "tsakirist/telescope-lazy.nvim" },
         },
         config = function()
