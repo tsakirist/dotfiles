11269e6345e47105459f5ed4713ea20e2541845b
Tsakiris Tryfon <tr.tsakiris@gmail.com>
2024-09-30 19:55:19 +0300

    Add `vim.api.keyset.keymap` to opts for `utils.map` and remove whitespace in docs

--- nvim/lua/tt/utils/init.lua
+++ nvim/lua/tt/utils/init.lua
@@ -6,11 +6,11 @@ M.is_windows = vim.uv.os_uname().sysname:match "Windows"
 --- Returns the appropriate path separator according to the current OS.
 M.path_separator = M.is_windows and "\\" or "/"
 
---- Creates new mapping(s).
----@param mode string|string[]: can be for example 'n', 'i', 'v' or { 'n', 'i', 'v' }
----@param lhs string|string[]: the left hand side
----@param rhs string|function: the right hand side
----@param opts table?: a table containing the mapping's options e.g. silent, remap
+---Creates new mapping(s).
+---@param mode string|string[]: The mode(s) of the mapping.
+---@param lhs string|string[]: The LHS of the mapping.
+---@param rhs string|function: The RHS of the mapping.
+---@param opts? vim.api.keyset.keymap: Table containing options for the mapping.
 function M.map(mode, lhs, rhs, opts)
     local options = { silent = true }
     if opts then
@@ -24,14 +24,14 @@ function M.map(mode, lhs, rhs, opts)
     end
 end
 
---- Joins the passed arguments with the appropriate file separator.
+---Joins the passed arguments with the appropriate file separator.
 ---@vararg any: The paths to join.
 ---@return string: The final joined path.
 function M.join_paths(...)
     return table.concat({ ... }, M.path_separator)
 end
 
---- Opens the given url in the default browser.
+---Opens the given url in the default browser.
 ---@param url string: The url to open.
 function M.open_in_browser(url)
     local open_cmd
@@ -55,21 +55,21 @@ function M.open_in_browser(url)
     end
 end
 
---- Trims leading and trailing whitespace from the string.
+---Trims leading and trailing whitespace from the string.
 ---@param s string: The string to be trimmed.
 ---@return string: The trimmed string.
 function M.trim(s)
     return s:match "^%s*(.-)%s*$"
 end
 
---- Returns whether we're currently inside a git repo.
+---Returns whether we're currently inside a git repo.
 ---@return boolean
 function M.is_in_git_repo()
     local result = vim.system({ "git", "rev-parse", "--is-inside-work-tree" }, { text = true }):wait()
     return result.stdout:match "true" ~= nil
 end
 
---- Returns the root path of the current git repository.
+---Returns the root path of the current git repository.
 ---@return string|nil
 local function get_git_root_internal()
     if M.is_in_git_repo() then
@@ -78,7 +78,7 @@ local function get_git_root_internal()
     end
 end
 
---- Returns the root path of the current git repository, adjusting path separators based on the OS..
+---Returns the root path of the current git repository, adjusting path separators based on the OS..
 ---@return string|nil
 function M.get_git_root()
     local git_root = get_git_root_internal()
