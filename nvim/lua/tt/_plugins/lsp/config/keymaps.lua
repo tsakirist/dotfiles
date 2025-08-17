3b35671e7c12f65c67fc6284ebc40d8b25060ad9
Tsakiris Tryfon <tr.tsakiris@gmail.com>
2025-08-17 22:37:04 +0300

    Fix deprecation warnings for `lsp.util.make_position_params`

--- nvim/lua/tt/_plugins/lsp/config/keymaps.lua
+++ nvim/lua/tt/_plugins/lsp/config/keymaps.lua
@@ -3,31 +3,36 @@ local M = {}
 local utils = require "tt.utils"
 
 local function hover_on_new_window()
-    vim.lsp.buf_request(0, "textDocument/hover", vim.lsp.util.make_position_params(), function(_, result, ctx, config)
-        config = config or {}
-        config.focus_id = ctx.method
+    vim.lsp.buf_request(
+        0,
+        "textDocument/hover",
+        vim.lsp.util.make_position_params(0, "utf-8"),
+        function(_, result, ctx, config)
+            config = config or {}
+            config.focus_id = ctx.method
 
-        -- Ignore result since buffer changed. This happens for slow language servers
-        if vim.api.nvim_get_current_buf() ~= ctx.bufnr then
-            return
+            -- Ignore result since buffer changed. This happens for slow language servers
+            if vim.api.nvim_get_current_buf() ~= ctx.bufnr then
+                return
+            end
+
+            -- No information available
+            if not (result and result.contents) then
+                return
+            end
+
+            local markdown_lines = vim.lsp.util.convert_input_to_markdown_lines(result.contents)
+
+            -- Open an new window with the hover information
+            vim.cmd.new()
+            vim.api.nvim_set_option_value("filetype", "markdown", { scope = "local" })
+            vim.api.nvim_set_option_value("buftype", "nofile", { scope = "local" })
+            vim.api.nvim_set_option_value("buflisted", false, { scope = "local" })
+            vim.api.nvim_buf_set_lines(0, 0, -1, false, markdown_lines)
+
+            utils.map("n", "q", "<C-w>c", { buffer = true })
         end
-
-        -- No information available
-        if not (result and result.contents) then
-            return
-        end
-
-        local markdown_lines = vim.lsp.util.convert_input_to_markdown_lines(result.contents)
-
-        -- Open an new window with the hover information
-        vim.cmd.new()
-        vim.api.nvim_set_option_value("filetype", "markdown", { scope = "local" })
-        vim.api.nvim_set_option_value("buftype", "nofile", { scope = "local" })
-        vim.api.nvim_set_option_value("buflisted", false, { scope = "local" })
-        vim.api.nvim_buf_set_lines(0, 0, -1, false, markdown_lines)
-
-        utils.map("n", "q", "<C-w>c", { buffer = true })
-    end)
+    )
 end
 
 function M.on_attach(_, bufnr)
