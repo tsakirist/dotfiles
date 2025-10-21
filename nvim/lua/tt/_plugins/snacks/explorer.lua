e878bfd1d27c0ed7121151295919815e38f86d2b
Tsakiris Tryfon <tr.tsakiris@gmail.com>
2025-10-21 21:46:12 +0300

    Enhnacements in `snacks.explorer`
    - Removed input window
    - Removed bound '/' keymap, now it searches in buffer
    - Opening a file will close the preview_win

--- nvim/lua/tt/_plugins/snacks/explorer.lua
+++ nvim/lua/tt/_plugins/snacks/explorer.lua
@@ -3,6 +3,20 @@ local M = {}
 ---Source code adapted from: https://github.com/folke/snacks.nvim/discussions/1306#discussioncomment-12248922
 ---@type snacks.picker.Config
 M.explorer = {
+    -- Layout with removed input field
+    layout = {
+        preview = "main",
+        layout = {
+            backdrop = false,
+            width = 40,
+            min_width = 40,
+            height = 0,
+            position = "left",
+            border = "none",
+            box = "vertical",
+            { win = "list", border = "none" },
+        },
+    },
     on_show = function(picker)
         local window_gap = 1
 
@@ -37,28 +51,46 @@ M.explorer = {
             end,
         }
 
-        picker.preview.win = preview_win
-
         root:on("WinResized", function()
             update(preview_win)
         end)
+
+        root:on("WinLeave", function()
+            vim.schedule(function()
+                if not picker:is_focused() then
+                    picker.preview.win:close()
+                end
+            end)
+        end)
+
+        picker.preview.win = preview_win
+        picker.main = preview_win.win
+    end,
+    on_close = function(picker)
+        picker.preview.win:close()
     end,
     actions = {
         toggle_preview = function(picker)
             picker.preview.win:toggle()
+        end,
+        open = function(picker, ...)
+            local explorer_actions = require "snacks.explorer.actions"
+            picker.preview.win:close()
+            explorer_actions.actions.confirm(picker, ...)
         end,
     },
     win = {
         list = {
             keys = {
                 ["-"] = "explorer_up",
-                ["o"] = "confirm",
-                ["="] = "confirm",
-                ["+"] = "confirm",
+                ["o"] = "open",
+                ["="] = "open",
+                ["+"] = "open",
                 ["O"] = "explorer_open",
                 ["?"] = "toggle_help_list",
                 ["<C-t>"] = "tab",
                 ["<M-h>"] = false,
+                ["/"] = false,
             },
         },
     },
