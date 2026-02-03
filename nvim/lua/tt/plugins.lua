3e923ba5382d677dadf508ed7977df9c13ef4ce6
Tsakiris Tryfon <tr.tsakiris@gmail.com>
2026-02-03 23:27:01 +0200

    Remove `checkmate`

--- nvim/lua/tt/plugins.lua
+++ nvim/lua/tt/plugins.lua
@@ -809,13 +809,6 @@ return {
         end,
     },
 
-    -- Simple and sophisticated Neovim todo plugin
-    {
-        "bngarren/checkmate.nvim",
-        ft = "markdown",
-        opts = {},
-    },
-
     -- Wrapper over UNIX shell commands
     {
         "chrisgrieser/nvim-genghis",
